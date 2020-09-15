6d5d19073ada107c9527505b5a6aacfc332c2ede
ADISH.A.P <helloadish007@gmail.com>
2020-09-15 18:23:58 +0530

    Add files via upload

---
+++ what is a module? how to create it?/testmodule.py
@@ -0,0 +1,9 @@
+#A module is  file containing a set of functions you want to include in your application.
+def helloworld():
+	print('hello world')
+def add(x,y):
+	return x+y
+
+name_of_author=['hello','adish','007']
+
+
